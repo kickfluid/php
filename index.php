10b8b1b91013ddaceae7d07242cefd647b0f61a8
Martial_GESLIN <martial.geslin@hotmail.com>
2021-11-29 15:13:17 +0100

    debut

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-Bonjour
+<html>
+
+	<body>
+		<form action="ajout.html"><button>Ajouter un patient</button></form>
+		<form action="modification.html"><button>modifier un patient</button></form>
+		<form action="suppresion.html"><button>supprimer un patient</button></form>
+
+		<?php
+
+		?>
+	</body>
+</html>
