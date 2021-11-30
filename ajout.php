38fc12c3cdba58a4cb16926d7aecc9719a63b181
Martial_GESLIN <martial.geslin@hotmail.com>
2021-11-30 08:40:51 +0100

    ajout

---
+++ ajout.php
@@ -0,0 +1,19 @@
+<html>
+	<body>
+		<form action="ajoutcontact.php" method="POST">
+
+			Nom :         <input type='text' name="nom" required /><br/>
+			Prenom :      <input type='text' name="prenom" required/><br/>
+			Adresse :     <input type='text' name="adresse" required/><br/>
+			Code Postal : <input type='text' name="cp" required/><br/>
+			Ville :       <input type='text' name="ville" required/><br/>
+			Civilité :         <input type='text' name="civilite" required/><br/>
+            Date de naissance :         <input type='text' name="tel" required/><br/>
+            Lieu de naissance :         <input type='text' name="tel" required/><br/>
+            Numéro de sécurité sociale :         <input type='text' name="tel" required/><br/>
+			<input type='reset' />
+			<input type='submit'/>
+
+		</form>
+	</body>
+</html>
