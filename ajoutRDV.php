dc374bfff0f95375498bf34fcb0a29e205145bd1
Martial_GESLIN <martial.geslin@hotmail.com>
2021-12-07 09:25:54 +0100

    priseRDV

--- ajoutRDV.php
+++ ajoutRDV.php
@@ -1,11 +1,40 @@
 <html>
+	RDV !
+<?php 
+		try { 
+			$linkpdo = new PDO("mysql:host=localhost;dbname=gestion", 'root',);
+			$linkpdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		} 
+		catch (Exception $e) { 
+			die('Erreur : ' . $e->getMessage()); 
+		}
+
+		$res = $linkpdo->prepare("SELECT * FROM usager WHERE id_usager=:id_usager");
+		$res->execute(array('id_usager' => isset($_GET['id_usager']) ? $_GET['id_usager'] : null));
+		$data = $res->fetch();
+
+		$res2 = $linkpdo->prepare("SELECT * FROM medecin");
+		$ret = $res2->execute();
+		if(!$ret){
+			echo 'Erreur';
+		}
+		?>
 	<body>
 		<form action="ajoutRDV.php" method="POST">
-
-			Nom du médecin :         <input type='text' name="nom" required /><br/>
-			Nom du patient :      <input type='text' name="prenom" required/><br/>
-			Date RDV :     <input type='text' name="adresse" required/><br/>
-			Heure : <input type='text' name="cp" required/><br/>
+			<?php echo $data['nom'].' '.$data['prenom'];?>
+			<br/>
+			<select name="id_medecin" required>
+							<option value="NULL" default>-- Médecin --</option>
+						<?php
+						while ($data2 = $res2->fetch()) {
+							echo'<option value ="'.$data2['id_medecin'].'">'.$data2['nom'].'</option>';
+						}
+						$res2->closeCursor();
+						?>
+					</select>
+					<br/>
+			Date RDV :     <input type='date' name="rdv" required/><br/>
+			Heure : <input type='time' name="duree" required/><br/>
 
 			<input type='reset' />
 			<input type='submit'/>
