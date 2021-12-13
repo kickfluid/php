3d6775e1700d5e576962badc359acb08db452f27
FlorianIUT <florian.chicot@etu.iut-tlse3.fr>
2021-12-13 15:02:32 +0100

    index stats

--- ajoutm.php
+++ ajoutm.php
@@ -2,12 +2,21 @@
 	<meta charset="UTF-8">
 	<body>
 		<form action="ajoutmedecin.php" method="POST">
-			Civilité : <input type='text' name="civilite" required/><br/>
+			<!-- Civilité : <input type='text' name="civilite" required/><br/> -->
+
+			Civilité : <select name="civilite" required>
+				<option value="NULL" default>-- Civilité --</option>
+				<option value="F" default> Femme </option>
+				<option value="M" default> Homme </option>
+				<option value="X" default> Autre </option>
+			</select><br/>
+
 			Nom :      <input type='text' name="nom" required /><br/>
 			Prenom :   <input type='text' name="prenom" required/><br/>
 			<input type='reset' />
 			<input type='submit'/>
 
 		</form>
+		<form action="index.php"><button>retour</button></form>
 	</body>
 </html>
--- index.php
+++ index.php
@@ -4,9 +4,11 @@
 		<form action="ajout.php"><button>Ajouter un patient</button></form>
 		<form action="rechercherusager.html"><button>rechercher un usager</button></form>
 
+		<form action="statistiquesp.php"><button>Statistiques patients</button></form>
 		
 		<form action="ajoutm.php"><button>Ajouter un médecin</button></form>
 		<form action="rechercherm.php"><button>Rechercher un médecin</button></form>
+
 
 		<?php
 
